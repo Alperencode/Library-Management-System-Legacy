f6c280822dedac3fd7fabbf0d37a0b3821453671
Alperen Aga <alperencode@gmail.com>
2023-05-24 01:19:16 +0300

    Fix SQLiteDB bugs

--- database/SQLiteDB.py
+++ database/SQLiteDB.py
@@ -1,5 +1,4 @@
 import sqlite3, re
-from classes.book import Book
 
 class SQLiteDataBase:
     def __init__(self, databaseName):
@@ -8,18 +7,28 @@ class SQLiteDataBase:
         self.cursor = self.conn.cursor()
 
     @staticmethod
-    def SanitizeName(tableName):
-        # Define the allowed characters for the table name
-        allowedCharacters = r'[a-zA-Z0-9_]+'
-        
-        # Remove any potentially harmful characters from the table name
-        sanitizedTableName = re.sub(r'[^a-zA-Z0-9_]', '', tableName)
-        
-        # Validate the table name
-        if re.fullmatch(allowedCharacters, sanitizedTableName):
-            return sanitizedTableName
-        else:
-            raise ValueError("Invalid table name.")
+    def SanitizeName(database_name):
+        # Remove invalid characters from the database name
+        sanitized_name = re.sub(r'[!@#$%^&*()+={}\[\]|\\\"\'<>.,/?~\n\r\t]', '', database_name)
+        sanitized_name = sanitized_name.strip()
+
+        # Empty string check
+        if not sanitized_name:
+            raise ValueError("Invalid database name. Name contains only invalid characters.")
+
+        # Reserved keyword check
+        reserved_keywords = [
+            "SELECT", "FROM", "WHERE", "INSERT", "UPDATE", "DELETE",  # Add more reserved keywords if needed
+        ]
+        if sanitized_name.upper() in ' '.join(reserved_keywords).upper():
+            raise ValueError("Invalid database name. Reserved keyword detected.")
+
+        # Starts with a digit check
+        if sanitized_name[0].isdigit():
+            raise ValueError("Invalid database name. Cannot start with a digit.")
+
+        # Valid name
+        return sanitized_name
     
     def ExecuteSQL(self, query, *parameters):
         if parameters:
@@ -30,7 +39,7 @@ class SQLiteDataBase:
 
     def AddToTable(self, tableName, parameters=None):
         if not parameters:
-            parameters = ()
+            return
         tableName = SQLiteDataBase.SanitizeName(tableName)
         query = "INSERT INTO {} VALUES ({})".format(
             tableName, ", ".join("?" * len(parameters))
